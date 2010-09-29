423b5848f12a0b2ccd491d8aae116bd9249d57ed
Andrew Liu <andrewliu33@gmail.com>
2010-09-29 11:57:58 +0800

    Ack will fallback to grep if not exists.

--- FindInProject/FindInProjectParser.py
+++ FindInProject/FindInProjectParser.py
@@ -2,6 +2,7 @@
     Parse the result of ack into html
 """
 
+import os
 import subprocess
 import cgi
 import re
@@ -9,10 +10,16 @@ import re
 class FindInProjectParser:
 
     def __init__(self, query, path):
-        arg = ['ack-grep', '-C', '2', '--color','--color-filename=reset', '--color-match=yellow', query]
-        process = subprocess.Popen(arg, stdout=subprocess.PIPE, cwd=path)
-        self.raw = cgi.escape(process.communicate()[0])
-        #print self.raw
+        if os.popen("which ack ack-grep").readlines():
+            arg = ['ack-grep', '-C', '2', '--color','--color-filename=reset', '--color-match=yellow', query]
+            process = subprocess.Popen(arg, stdout=subprocess.PIPE, cwd=path)
+            self.raw = cgi.escape(process.communicate()[0])
+            self.raw = self.raw.replace('\x1b[0m\x1b[K','')
+        else:
+            arg = ['grep', '-R', '-n', '-H', '-I', '-C', '2', query, '.', '--color=force']
+            process = subprocess.Popen(arg, stdout=subprocess.PIPE, cwd=path,env={"GREP_COLORS": "ms=33:mc=01;31:sl=:cx=:fn=0:ln=:bn=32:se="})
+            self.raw = cgi.escape(process.communicate()[0])
+            self.raw = self.raw.replace('\x1b[K', '')
 
     def html(self):
         blocks = self.__tuple()
@@ -30,7 +37,7 @@ class FindInProjectParser:
         </tr>
     </thead>
     <tbody>
-            """ % block[0][0]
+            """ % os.path.normpath(block[0][0])
             for line in block:
                 matchclass = ""
                 if line[2]:
@@ -55,16 +62,14 @@ class FindInProjectParser:
         #\x1b[0mew\x1b[0m-67-    Eastwind()
         #\x1b[0mew\x1b[0m-68-
 
-        # strip empty highlight
-        process = self.raw.replace('\x1b[0m\x1b[K','')
-        groups = process.split('--')
+        groups = self.raw.split('--')
         lines = [[self.__metadata(l) for l in g.split('\n') if l != ''] for g in groups]
         return lines
 
     def __metadata(self, line):
-        match = re.match("^\\x1b\[0m(.*?)\\x1b\[0m[:-](\d+)([:-])(.*)", line)
+        match = re.match("^\\x1b\[0m(.*?)\\x1b\[0?m[:-](\d+)([:-])(.*)", line)
         matched = (match.group(3) == ':')
         clear = match.group(4).replace(' ', '&nbsp;')
-        clear = re.sub("\\x1b\[33m(.*?)\\x1b\[0m", '<span class="highlight">\\1</span>', clear)
+        clear = re.sub("\\x1b\[33m(.*?)\\x1b\[0?m", '<span class="highlight">\\1</span>', clear)
         return (match.group(1), match.group(2), matched, clear)
 
