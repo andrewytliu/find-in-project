f912606e4aa4ef5db6c4d4673ce8f5e585511be0
Andrew Liu <andrewliu33@gmail.com>
2010-10-07 22:52:34 +0800

    Fix duplicated result in search suggestions.

--- FindInProject/FindInProjectWindow.py
+++ FindInProject/FindInProjectWindow.py
@@ -100,6 +100,7 @@ class FindInProjectWindow:
         self._extbox.connect("icon-release", self.box_clear)
         self._extbox.connect("key-release-event", self.box_key)
         self._spinner = self._builder.get_object("spinner")
+        self._searched = []
 
     def init(self):
         self._window.deiconify()
@@ -140,7 +141,9 @@ class FindInProjectWindow:
         self._spinner.start()
         gtk.gdk.window_process_all_updates()
         self._path = filebrowser_root()
-        self._history.set(self._history.append(), 0, query)
+        if not query in self._searched:
+            self._history.set(self._history.append(), 0, query)
+            self._searched.append(query)
         parser = FindInProjectParser(query, url2pathname(self._path)[7:], context=self._show_context.get_active(), regex=self._use_regex.get_active(), ignorecase=self._ignore_case.get_active(), filetype=self._extbox.get_text())
         self._browser.load_string(style_str + parser.html(), "text/html", "utf-8", "about:")
         self._message.set_text('%d line(s) matched in %d file(s)' % parser.status())
